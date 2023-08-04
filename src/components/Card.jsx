f6e3c0d21e1df9817af696babe856017319c5fb7
Ashish Sahu <itsashishsahu.inc@gmail.com>
2023-08-04 20:02:19 +0530

    * Update -
    1. Removed some files from src folder.
    2. Added components folder.
    3. Created Card.jsx and Modal.jsx component.
    4. Added CSS  for Card.jsx and Modal.jsx as card.css and modal.css

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import './card.css'
+
+const Card = ({children, className, onClick}) => {
+
+    
+  return (
+    <article className={`card ${className}`} onClick={onClick}>
+        {children}
+    </article>
+  )
+}
+
+export default Card
