48e208ba4468412cd1944b7a4bb76433b80a1d35
Ashish Sahu <itsashishsahu.inc@gmail.com>
2023-08-04 20:24:12 +0530

    * Updates -
    1. Major updates in section folders.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./sections/navbar/Navbar"
+import Header from "./sections/header/Header"
+import About from "./sections/about/About"
+import Services from "./sections/services/Services"
+import Testimonials from "./sections/testimonials/Testimonials"
+import FAQs from "./sections/faqs/FAQs"
+import Contact from "./sections/contact/Contact"
+import Footer from "./sections/footer/Footer"
+import FloatingNav from "./sections/floating-nav/FloatingNav"
+import Portfolio from "./sections/portfolio/Portfolio"
+import Modal from "./components/Modal"
+import Theme from "./theme/Theme"
+import { useThemeContext } from "./context/theme-context"
+
+
 
 function App() {
+  
+  const {themeState} = useThemeContext();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className={`${themeState.primary} ${themeState.background}`}>
+        <Navbar/>
+        <Header/>
+        <About/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
+        <FloatingNav/>
+        <Theme/>
+    </main>
+  )
 }
 
-export default App;
+export default App
